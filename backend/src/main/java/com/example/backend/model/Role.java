e2082dfe3bd8ef9bf874a6889d0abca8e4a53d22
Kheder Murad <kheder99@gmx.de>
2025-02-01 22:20:49 +0100

    Backend: Add Role data model

---
+++ backend/src/main/java/com/example/backend/model/Role.java
@@ -0,0 +1,19 @@
+package com.example.backend.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+}
