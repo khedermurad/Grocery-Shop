a6671d937eabf08ce23dba392b0930dbe4232789
Kheder Murad <kheder99@gmx.de>
2025-02-03 17:36:32 +0100

    Backend: Add security config class

---
+++ backend/src/main/java/com/example/backend/security/SecurityConfig.java
@@ -0,0 +1,56 @@
+package com.example.backend.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
+        // hier anpassen
+        httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .exceptionHandling(customizer ->
+                        customizer.authenticationEntryPoint(authEntryPoint))
+                .sessionManagement(customizer ->
+                        customizer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(request ->
+                        request.requestMatchers("/api/auth/**").permitAll().anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults());
+
+        httpSecurity.addFilterBefore(jwtAuthnticationFilter(), UsernamePasswordAuthenticationFilter.class);
+        return httpSecurity.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception{
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public JWTAuthnticationFilter jwtAuthnticationFilter(){
+        return new JWTAuthnticationFilter();
+    }
+
+
+
+
+}
