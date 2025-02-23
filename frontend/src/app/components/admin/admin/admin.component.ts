580255ac4586a5a660aed1a0d714ffbf9c7bc030
Kheder Murad <kheder99@gmx.de>
2025-02-23 16:33:22 +0100

    Frontend: Update sidenav & add menu bar

--- frontend/src/app/components/admin/admin/admin.component.ts
+++ frontend/src/app/components/admin/admin/admin.component.ts
@@ -1,13 +1,31 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import {MatSidenavModule} from '@angular/material/sidenav'; 
-
+import { Router, RouterModule } from '@angular/router';
+import { MatSidenavModule } from '@angular/material/sidenav'; 
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
+import { AuthService } from '../../../services/auth/auth.service';
 @Component({
+  standalone: true,
   selector: 'app-admin',
-  imports: [RouterModule, MatSidenavModule],
+  imports: [RouterModule, MatSidenavModule, MatIconModule, MatListModule, MatButtonModule, MatToolbarModule, MatMenuModule],
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.css'
 })
 export class AdminComponent {
+
+  constructor(private authService: AuthService, private router: Router){}
+
+
+  onSignOut(): void{
+    this.authService.logOut();
+    this.router.navigate(['/login']);
+  }
+
+  openDashBoard(): void{
+    this.router.navigate(['/admin']);
+  }
 
 }
